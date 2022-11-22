651b3d316f2ceec3efc3049cd2a17499f9c0deb2
Wendy C. Kurniawan <47620821+wendycahya@users.noreply.github.com>
2022-11-22 18:33:42 +0800

    update

--- Class-python/fuzzy-concept.py
+++ Class-python/fuzzy-concept.py
@@ -1,19 +1,20 @@
 from simpful import *
-
+import matplotlib.pylab as plt
+from numpy import linspace, array
 # A simple fuzzy inference system for the tipping problem
 # Create a fuzzy system object
 FS = FuzzySystem()
 
 # Define fuzzy sets and linguistic variables
-S_1 = FuzzySet(function=Sigmoid_MF(c=100, a=0.01), term="positive")
-S_2 = FuzzySet(function=InvSigmoid_MF(c=-100, a=0.01), term="negative")
-input1 = LinguisticVariable([S_1, S_2], concept="Derivative Distance", universe_of_discourse=[-500, 500])
+S_1 = FuzzySet(function=Sigmoid_MF(c=300, a=0.05), term="positive")
+S_2 = FuzzySet(function=InvSigmoid_MF(c=300, a=0.05), term="negative")
+input1 = LinguisticVariable([S_1, S_2], concept="Derivative Distance", universe_of_discourse=[0, 600])
 FS.add_linguistic_variable("TimeDistance", input1)
 input1.plot()
 
-F_1 = FuzzySet(function=Sigmoid_MF(c=100, a=0.01), term="positive")
-F_2 = FuzzySet(function=InvSigmoid_MF(c=-100, a=0.01), term="negative")
-input2 = LinguisticVariable([F_1, F_2], concept="Scalar Product", universe_of_discourse=[-500, 500])
+F_1 = FuzzySet(function=Sigmoid_MF(c=300, a=0.05), term="positive")
+F_2 = FuzzySet(function=InvSigmoid_MF(c=300, a=0.05), term="negative")
+input2 = LinguisticVariable([F_1, F_2], concept="Scalar Product", universe_of_discourse=[0, 600])
 FS.add_linguistic_variable("ScalarProduct", input2)
 input2.plot()
 
@@ -33,8 +34,40 @@ R4 = "IF (TimeDistance IS positive) OR (ScalarProduct IS positive) THEN (AlphaVa
 FS.add_rules([R1, R2, R3, R4])
 #
 # # Set antecedents values
-FS.set_variable("TimeDistance", 50)
-FS.set_variable("ScalarProduct", -50)
+FS.set_variable("TimeDistance", 450)
+FS.set_variable("ScalarProduct", 600)
 
 # Perform Mamdani inference and print output
 print(FS.Mamdani_inference(["AlphaValue"]))
+
+# Plotting surface
+xs = []
+ys = []
+zs = []
+DIVs = 50
+for x in linspace(0, 600, DIVs):
+    for y in linspace(0, 600, DIVs):
+        FS.set_variable("TimeDistance", x)
+        FS.set_variable("ScalarProduct", y)
+        tip = FS.inference()['AlphaValue']
+        xs.append(x)
+        ys.append(y)
+        zs.append(tip)
+xs = array(xs)
+ys = array(ys)
+zs = array(zs)
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+xx, yy = plt.meshgrid(xs, ys)
+
+ax.plot_trisurf(xs, ys, zs, vmin=0, vmax=1, cmap='gnuplot2')
+ax.set_xlabel("Time Distance")
+ax.set_ylabel("Scalar Product")
+ax.set_zlabel("Alpha Product")
+ax.set_title("Human Robot Collaboration", pad=50)
+ax.set_zlim(0, 1)
+plt.tight_layout()
+plt.show()
