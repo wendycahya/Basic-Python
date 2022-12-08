c9bc0c95d2445b95a0580e6c7df3454fe71b8426
Wendy C. Kurniawan <47620821+wendycahya@users.noreply.github.com>
2022-12-08 11:18:02 +0800

    update

---
+++ Class-python/basic-algoritma.py
@@ -0,0 +1,4 @@
+#1. Multitread code 1) menjalankan robot bekerja, 2) menjalankan kamera
+#2. Robot bekerja berdasarkan pre-trajectory
+#3. Camera dijalankan 1) mendeteksi manusia, 2) mendeteksi skeleton, 3) memberikan informasi posisi manusia, 4) menurunkan menjadi velocity dan acceleration
+#4. Perubahan robot
